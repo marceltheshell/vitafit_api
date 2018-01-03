bedea587135e3fabe712ee0a8607f241ca6404c0
marceltheshell <marcel.degas@gmail.com>
2018-01-03 00:34:34 -0800

    adds tests for the blogpost model

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ group :development, :test do
 end
 
 group :development do
-  gem 'rspec'
+  gem 'rspec-rails'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -1,10 +1,11 @@
-require "spec_helper"
+require "rails_helper"
 
-describe BlogPost, type: :model do
+describe BlogPost do
 
   let (:blog_post) {BlogPost.create!(title: "first!")}
 
-  describe 'testing blogpost model' do
-    it 'should have a valid title'
+  it 'should have a valid title' do 
+    expect(blog_post.title).to eq("first!")
   end
+
 end
