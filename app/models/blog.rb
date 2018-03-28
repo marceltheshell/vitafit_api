fa04cba75995acf34968b65d800d152a7beea4de
marceltheshell <marcel.degas@gmail.com>
2018-03-28 12:23:25 -0700

    updates models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :author, presence: true
+  validates :views, presence: true
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
 end
