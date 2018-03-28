3874c9d42c2627f8f59245df2ce06244e85f450d
marceltheshell <marcel.degas@gmail.com>
2018-03-28 11:47:00 -0700

    adds the logic for mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,14 @@
 class ContactMailer < ApplicationMailer
+  default from: 'mayratn.hall@gmail.com'
+
+  def response_email(contact_id)
+    contact = Contact.find(contact_id)
+
+    mail(   :to      => contact.email
+            :subject => 'Meeting your health goals with VitaFit'
+    ) do |format|
+      format.text
+      format.html
+    end
+  end
 end
