2e9bb480f6708c0561f5f45d5b29da13f2889a1e
marceltheshell <marcel.degas@gmail.com>
2018-04-01 14:02:30 -0700

    confirgures rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://vitafitpt.herokuapp.com/'
+    origins 'localhost:4200'
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
+  end
+
+  allow do
+    origins 'https://vitafitpt.herokuapp.com'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
